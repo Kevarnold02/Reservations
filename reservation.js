30b2d1b3e8179977d380ba1feee21b103a81fb69
kavarnold02 <kevarnold02@gmail.com>
2016-04-30 13:17:05 -0400

    added html pages from Paul

--- reservation.js
+++ reservation.js
@@ -1,8 +1,21 @@
 // Dependencies
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+//require MySQL
+var mysql = require('mysql');
+
+//connect to mySQL database
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'Reservations',
+  multipleStatements : true
+});
+
+connection.connect();
+//database query, customer view
 
 // Sets up the Express App
 
@@ -29,10 +42,73 @@ var customers = [
 // Routes
 // Basic route that sends the user first to the AJAX Page
 app.get('/', function(req, res){
-	
+	// connection.query('SELECT * FROM customers', function(err, results) {
+	//   if (err) throw err;
+	 
+	//   for (var i = 0; i < results.length; i++) {
+
+	//   }
+	console.log(__dirname);
 	//res.send("Welcome to the Star Wars Page!")
 	res.sendFile(path.join(__dirname + '/view.html'));
+
+
 })
+
+// Create New Characters - takes in JSON input
+// Going to let customers enter their information to get a table.
+app.post('/api/new', function(req, res){
+
+	// req.body hosts is equal to the JSON post sent from the user
+	var newcharacter = req.body;
+
+	console.log(newcharacter);
+
+	// We then add the json the user sent to the character array
+	characters.push(newcharacter);
+
+	// We then display the JSON to the users
+	res.json(newcharacter);
+})
+
+// // $(".submit").on("click", function(){
+
+// // 	// Here we grab the form elements
+// // 	var newReservation = {
+// // 		customerName: $('#reserve_name').val().trim(),
+// // 		phoneNumber: $('#reserve_phone').val().trim().replace(/-/g,""),
+// // 		customerEmail: $('#reserve_email').val().trim(),
+// // 		customerID: $('#reserve_uniqueID').val().trim()
+// // 	};
+
+// // 	console.log(newReservation);
+
+// // 			var currentURL = window.location.origin;
+
+// // 		    $.post(currentURL + "/api/tables", newReservation,
+// // 		    function(data){
+
+// // 		    	// If a table is available... tell user they are booked.
+// // 		    	if(data == true){
+// // 		    		alert("Yay! You are officially booked!")
+// // 		    	}
+
+// // 		    	// If a table is available... tell user they on the waiting list.
+// // 		    	if(data == false){
+// // 		    		alert("Sorry you are on the wait list")
+// // 		    	}
+
+// // 		    	// Clear the form when submitting
+// // 	    		$('#reserve_name').val("");
+// // 				$('#reserve_phone').val("");
+// // 				$('#reserve_email').val("");
+// // 				$('#reserve_uniqueID').val("");
+
+// // 		    });
+
+// // 	return false;
+
+// 	});
 
 // Search for Specific Character (or all characters) - provides JSON
 //Need to change this to show tables, not search for customers
@@ -59,21 +135,7 @@ app.get('/api/:customers?', function(req, res){
 	}
 })
 
-// Create New Characters - takes in JSON input
-// Going to let customers enter their information to get a table.
-app.post('/api/new', function(req, res){
 
-	// req.body hosts is equal to the JSON post sent from the user
-	var newcharacter = req.body;
-
-	console.log(newcharacter);
-
-	// We then add the json the user sent to the character array
-	characters.push(newcharacter);
-
-	// We then display the JSON to the users
-	res.json(newcharacter);
-})
 
 // Starts the server to begin listening 
 
