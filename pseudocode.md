cf47e496afe5180d39dd7eda3578360e19dc5e05
kavarnold02 <kevarnold02@gmail.com>
2016-04-30 14:20:32 -0400

    half working, mostly not working

---
+++ pseudocode.md
@@ -0,0 +1,17 @@
+base pseudo code:
+
+Create the front-end (visuals) for home page, reservation form, and reservation views.
+	using a bootstrap theme for the user interface html page.	
+
+Create a basic server using Express.JS
+	server.js will contain connections and routes to take reservations for a restaurant
+
+Create a few array variables that will hold the data
+	[{},{}]
+
+Create a set of routes for getting and posting table data
+	
+
+Create a set of routes for displaying the HTML pages
+
+Use jQuery to run AJAX calls to GET and POST data from users to the Express server
--- reservation.js
+++ reservation.js
@@ -32,10 +32,10 @@ app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
 var customers = [
 	{
-		customerName: "",
-		phoneNumber: "",
-		customerEmail: "",
-		customerID: ""
+		customer_name: "",
+		phone_number: "",
+		customer_email: "",
+		id: ""
 	}
 ]
 
@@ -48,13 +48,51 @@ app.get('/', function(req, res){
 	//   for (var i = 0; i < results.length; i++) {
 
 	//   }
-	console.log(__dirname);
+	// console.log(__dirname);
 	//res.send("Welcome to the Star Wars Page!")
 	res.sendFile(path.join(__dirname + '/view.html'));
 
 
 })
+app.get('/view', function(req, res){
+	// connection.query('SELECT * FROM customers', function(err, results) {
+	//   if (err) throw err;
+	 
+	//   for (var i = 0; i < results.length; i++) {
 
+	//   }
+	// console.log(__dirname);
+	//res.send("Welcome to the Star Wars Page!")
+	res.sendFile(path.join(__dirname + '/view.html'));
+
+
+})
+app.get('/reserve', function(req, res){
+	connection.query('INSERT INTO customers (customer_name, customer_email, phone_number) VALUES ('+customerName+','+customerEmail+','+phoneNumber+');', function(err, results) {
+	  if (err) throw err;
+	 
+	  for (var i = 0; i < results.length; i++) {
+
+	  }
+	console.log(__dirname);
+	//res.send("Welcome to the Star Wars Page!")
+	res.sendFile(path.join(__dirname + '/reserve.html'));
+
+
+})
+app.get('/tables', function(req, res){
+	// connection.query('SELECT * FROM customers', function(err, results) {
+	//   if (err) throw err;
+	 
+	//   for (var i = 0; i < results.length; i++) {
+
+	//   }
+	console.log(__dirname);
+	//res.send("Welcome to the Star Wars Page!")
+	res.sendFile(path.join(__dirname + '/tables.html'));
+
+
+})
 // Create New Characters - takes in JSON input
 // Going to let customers enter their information to get a table.
 app.post('/api/new', function(req, res){
@@ -65,50 +103,13 @@ app.post('/api/new', function(req, res){
 	console.log(newcharacter);
 
 	// We then add the json the user sent to the character array
-	characters.push(newcharacter);
+	//characters.push(newcharacter);
 
 	// We then display the JSON to the users
 	res.json(newcharacter);
 })
 
-// // $(".submit").on("click", function(){
 
-// // 	// Here we grab the form elements
-// // 	var newReservation = {
-// // 		customerName: $('#reserve_name').val().trim(),
-// // 		phoneNumber: $('#reserve_phone').val().trim().replace(/-/g,""),
-// // 		customerEmail: $('#reserve_email').val().trim(),
-// // 		customerID: $('#reserve_uniqueID').val().trim()
-// // 	};
-
-// // 	console.log(newReservation);
-
-// // 			var currentURL = window.location.origin;
-
-// // 		    $.post(currentURL + "/api/tables", newReservation,
-// // 		    function(data){
-
-// // 		    	// If a table is available... tell user they are booked.
-// // 		    	if(data == true){
-// // 		    		alert("Yay! You are officially booked!")
-// // 		    	}
-
-// // 		    	// If a table is available... tell user they on the waiting list.
-// // 		    	if(data == false){
-// // 		    		alert("Sorry you are on the wait list")
-// // 		    	}
-
-// // 		    	// Clear the form when submitting
-// // 	    		$('#reserve_name').val("");
-// // 				$('#reserve_phone').val("");
-// // 				$('#reserve_email').val("");
-// // 				$('#reserve_uniqueID').val("");
-
-// // 		    });
-
-// // 	return false;
-
-// 	});
 
 // Search for Specific Character (or all characters) - provides JSON
 //Need to change this to show tables, not search for customers
